6fb0e84d0146bbdb0fbdf6197c4fc029ef2c36e5
SrinivasJR <srinivasshetty.sjr@gmail.com>
2026-08-13 00:18:28 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -107,12 +107,10 @@ async function startServer() {
 
     await connectDatabase();
 
-    app.listen(PORT, () => {
-
+    app.listen(PORT, "0.0.0.0", () => {
         console.log(
-            `Server running on http://localhost:${PORT}`
+            `Server running on port ${PORT}`
         );
-
     });
 
 }
